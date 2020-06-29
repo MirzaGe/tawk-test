30a59bbe4fe97b7b1d5919580bc7a77e5b3167ea
john roque jorillo <jorillojohnroque@gmail.com>
2020-06-30 00:54:36 +0800

    Add UsersGateway unittest

--- GitHubAppTests/Gateways/UsersGatewayTests.swift
+++ GitHubAppTests/Gateways/UsersGatewayTests.swift
@@ -8,19 +8,54 @@
 
 import XCTest
 
+@testable import GitHubApp
+
 class UsersGatewayTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+        
+    }
+    
+    func test_get_users() {
+        let apiClient = ApiClientMock()
+        let sut = ApiUserGatewayImpl(apiClient: apiClient)
+        
+        let params = GetUsersParameters(since: 0)
+        
+        sut.getUsers(params: params) { (_) in }
+        
+        XCTAssertTrue(apiClient.executeCalled)
+        
+        let getUsersUrl = GetUsersApiRequest(params: params)
+        
+        XCTAssertEqual(apiClient.inputRequest?.url, getUsersUrl.urlRequest.url)
+    }
+    
+    private func makeSUT() -> ApiUserGatewayImpl {
+        
+        let apiClient = ApiClientMock()
+        let userGateway = ApiUserGatewayImpl(apiClient: apiClient)
+        
+        return userGateway
+        
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+class ApiClientMock: ApiClient {
+    
+    var inputRequest: URLRequest?
+    var executeCalled = false
+    
+    func execute<T>(request: ApiRequest, completionHandler: @escaping (Result<ApiResponse<T>, Error>) -> Void) where T : Decodable {
+        inputRequest = request.urlRequest
+        executeCalled = true
+        
     }
-
+    
 }
